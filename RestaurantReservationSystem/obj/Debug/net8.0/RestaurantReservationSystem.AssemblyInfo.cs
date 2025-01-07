58fe29357d4aa25e00fe9d633013e36fbc3e926d
Simeoncoded <simeonolawore07@gmail.com>
2025-01-06 20:23:20 -0500

    updated flow of app with persisting data with partial views updateed

--- RestaurantReservationSystem/obj/Debug/net8.0/RestaurantReservationSystem.AssemblyInfo.cs
+++ RestaurantReservationSystem/obj/Debug/net8.0/RestaurantReservationSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d800f15148e759daa380d06fffc0d04bfdc964")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3920af783eae2e142678f67b34b09f32128d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
