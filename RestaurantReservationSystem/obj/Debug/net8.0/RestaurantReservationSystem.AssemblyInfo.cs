6acd73b94b8ac23296ffa6325359fb924c747dce
Simeoncoded <simeonolawore07@gmail.com>
2025-01-19 13:25:06 -0500

    live chat updated

--- RestaurantReservationSystem/obj/Debug/net8.0/RestaurantReservationSystem.AssemblyInfo.cs
+++ RestaurantReservationSystem/obj/Debug/net8.0/RestaurantReservationSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff2405faf6c7e699792f924012ae4ceb47dc460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b833ce0aac53111133b373fea06c11a7fb9d368e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
