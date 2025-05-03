b1f108cdab48c860a48bea8302249179a0be2754
Simeoncoded <simeonolawore07@gmail.com>
2025-05-02 23:24:32 -0400

    update on table style

--- RestaurantReservationSystem/obj/Debug/net8.0/RestaurantReservationSystem.AssemblyInfo.cs
+++ RestaurantReservationSystem/obj/Debug/net8.0/RestaurantReservationSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86752ba19eb684f2f7d184dc7bc6b9390361b565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba9d7cc45f714e70cc41c15aa3f36483ca07c16")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
